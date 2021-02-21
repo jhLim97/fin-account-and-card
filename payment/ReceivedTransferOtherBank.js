9826923523567eee57d26b425b677ba345a7f6ec
jhLim97 <wnsgur2749@naver.com>
2021-02-21 18:57:16 +0900

    타행입금이체 실습

---
+++ payment/ReceivedTransferOtherBank.js
@@ -0,0 +1,37 @@
+
+
+
+// nh open API 타행입금이체
+var request = require('request');
+
+var option = {
+    url: 'https://developers.nonghyup.com/ReceivedTransferOtherBank.nh',
+    method:'POST',
+    json: true,
+    body:{
+      "Header": {
+        "ApiNm": "ReceivedTransferOtherBank",
+        "Tsymd": "20210221",
+        "Trtm": "184500",
+        "Iscd": "기관번호",
+        "FintechApsno": "001",
+        "ApiSvcCd": "ReceivedTransferA",
+        "IsTuno": "거래 고유 번호",
+        "AccessToken": "사용자 접근 토큰"
+      },
+      "Bncd": "은행코드",
+      "Acno": "계좌번호",
+      "Tram": "거래금액",
+      "DractOtlt": "테스트",
+      "MractOtlt": "테스트"
+    }
+}
+
+
+request(option, function(error, response, body) {
+    if (error) {
+        console.log(err);
+    } else {
+        console.log(body);
+    }
+});
